084059cdacb84f2cfda0cb8151a4c871e43c95f9
Roberto Benitez <rbenitez22 at yahoo dot com>
2015-12-03 16:14:09 -0600

    updated createBody to use template approach

--- src/org/lucleroy/fontawesome/palette/IconSnippet.java
+++ src/org/lucleroy/fontawesome/palette/IconSnippet.java
@@ -8,8 +8,15 @@ public abstract class IconSnippet extends AbstractSnippet {
     @Override
     protected String createBody() {
         Preferences pref = NbPreferences.forModule(FontAwesomePalettePanel.class);
-        String tag = pref.get("tag", "span");
-        return "<" + tag + " class=" + quote(getClasses()) + "></" + tag + ">";
+        String tag = pref.get("template", Utils.DEFAULT_TEMPLATE);
+        String var="${" + Utils.VAR_NAME + "}";
+        if(!tag.contains(var))
+        {
+            Utils.showErrorMessage("Invalid template");
+            return "";
+        }
+
+        return tag.replace(var, getClasses());
     }
 
     private String quote(String string) {
